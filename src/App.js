7c80675001a5d2236463d0a6ca470b2241947d54
Ondřej Garncarz <ondrej@garncarz.cz>
2018-11-01 04:59:56 +0100

    More OOP.

--- src/App.js
+++ src/App.js
@@ -10,10 +10,84 @@ var numerals = [
 ];
 
 
+class Numeral {
+
+  constructor(nr, fem_tr, fem_he, masc_tr='', masc_he='') {
+    this.nr = nr;
+    this.fem_tr = fem_tr;
+    this.fem_he = fem_he;
+    this.masc_tr = masc_tr;
+    this.masc_he = masc_he;
+  }
+
+  newQuestion() {
+    this.from_eng = Math.random() >= 0.5;
+    this.ok = false;
+  }
+
+  getQuestion() {
+    if (this.from_eng) {
+      return this.nr;
+    }
+    if (Math.random() >= 0.5 || this.masc_he === '') {
+      return this.fem_he + '(' + this.fem_tr + ')';
+    } else {
+      return this.masc_he + '(' + this.masc_tr + ')';
+    }
+  }
+
+  checkAnswer(answer) {
+    this.ok = false;
+    if (!answer) {
+      return false;
+    }
+    if (!this.from_eng) {
+      return answer === this.nr.toString();
+    }
+    if (this.fem_tr === answer || this.fem_he === answer) {
+      this.ok = 'fem';
+    }
+    if (this.masc_tr === answer || this.masc_he === answer) {
+      this.ok = 'masc';
+    }
+    return this.ok;
+  }
+
+  getHelp() {
+    if (this.ok === 'fem') {
+      return this.fem_he + '(' + this.fem_tr + ')';
+    }
+    if (this.ok == 'masc') {
+      return this.masc_he + '(' + this.masc_tr + ')';
+    }
+  }
+
+}
+
+
+class Numerals {
+
+  constructor() {
+    this.numerals = [];
+    for (var i = 0; i < numerals.length; i++) {
+      this.numerals.push(new Numeral(...numerals[i]));
+    }
+  }
+
+  newQuestion() {
+    this.numeral = this.numerals[Math.floor(Math.random() * this.numerals.length)];
+    this.numeral.newQuestion();
+  }
+
+}
+
+
 class App extends Component {
 
   constructor(props) {
     super(props);
+
+    this.numerals = new Numerals();
 
     this.newQuestion = this.newQuestion.bind(this);
     this.checkAnswer = this.checkAnswer.bind(this);
@@ -28,28 +102,26 @@ class App extends Component {
   }
 
   newQuestion() {
-    this.numeral = numerals[Math.floor(Math.random() * numerals.length)];
-    this.question = Math.floor(Math.random() * this.numeral.length);
+    this.numerals.newQuestion();
 
-    this.questionRef.current.innerHTML = this.numeral[this.question];
+    this.questionRef.current.innerHTML = this.numerals.numeral.getQuestion();
     this.inputRef.current.value = '';
     this.helpRef.current.innerHTML = '';
   }
 
   checkAnswer(event) {
     var answer = this.inputRef.current.value.trim();
-    var ok, msg = '';
+    var ok = this.numerals.numeral.checkAnswer(answer);
 
-    if (this.question === 0) {
-      ok = this.numeral.indexOf(answer) > 0;
-    } else {
-      ok = this.numeral[0].toString() === answer;
+    var msg = ok ? 'OK!' : 'No...';
+    var help = this.numerals.numeral.getHelp();
+
+    if (help) {
+      msg += '<br/>' + help;
     }
 
-    if (ok) {
-      msg = 'OK!';
-    } else if (event.target !== this.inputRef.current) {
-      msg = 'No...';
+    if (!ok && event.target === this.inputRef.current) {
+      msg = '';
     }
 
     this.helpRef.current.innerHTML = msg;
