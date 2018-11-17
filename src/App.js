209ba26b4bddd29a7b3b09de1747138e885e3cce
Ondřej Garncarz <ondrej@garncarz.cz>
2018-11-17 04:10:39 +0100

    Back to BrowserRouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
 import ReactGA from 'react-ga';
 
